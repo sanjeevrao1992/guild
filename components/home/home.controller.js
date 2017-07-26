baeea8d6af5b1fadaa8a2490cfba4e933846424d
sanjeev rao <sanjeev rao>
2017-07-26 11:01:37 +0530

    day2 commit

--- components/home/home.controller.js
+++ components/home/home.controller.js
@@ -1,8 +1,20 @@
 angular.module('GuildMockup')
-.controller(HomeController);
+.controller('HomeController', HomeController);
 
 HomeController.$inject = [];
 
 function HomeController() {
+	console.log("Home Controller is getting called");
 	var ctrl = this;
+	ctrl.homeShow = true
+	console.log(ctrl.homeShow);
+
+	ctrl.show = function () {
+		ctrl.homeShow = true;
+	};
+
+	ctrl.noShow = function () {
+		ctrl.homeShow = false;
+		console.log(ctrl.homeShow);
+	};
 }
