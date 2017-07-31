f755f8d6884e03c5ce766426881420caac50220c
sanjeev rao <sanjeev rao>
2017-07-31 08:49:21 +0530

    github changes

--- config.js
+++ config.js
@@ -9,30 +9,30 @@
 				url: '/',
 				controller: HomeController,
 				controllerAs: 'ctrl',
-				templateUrl: '/components/home/home.template.html'
+				templateUrl: 'components/home/home.template.html'
 			})
-			
+
 			.state('home.team', {
 				url: '/team/{id}',
-				templateUrl: '/components/team/team.template.html',
+				templateUrl: 'components/team/team.template.html',
 				controller: TeamController,
 				controllerAs: 'ctrl'
 			})
 			.state('home.about', {
 				url: '/about/{id}',
-				templateUrl: '/components/about/about.template.html',
+				templateUrl: 'components/about/about.template.html',
 				controller: AboutController,
 				controllerAs: 'ctrl'
 			})
 			.state('home.portfolio', {
 				url: '/portfolio/{id}',
-				templateUrl: '/components/portfolio/portfolio.template.html',
+				templateUrl: 'components/portfolio/portfolio.template.html',
 				controller: PortfolioController,
 				controllerAs: 'ctrl'
 			})
 			.state('home.contact', {
 				url: '/contact/{id}',
-				templateUrl: '/components/contact/contact.template.html',
+				templateUrl: 'components/contact/contact.template.html',
 				controller: ContactController,
 				controllerAs: 'ctrl'
 			});
